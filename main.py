ce3bbad946844ff7d9611baa543fd778d6751fe0
Canthar90 <d.cieslak90@gmail.com>
2023-01-04 13:40:59 +0100

    Added more endpoint for all historical data from weather station and historical data from station form given year

--- main.py
+++ main.py
@@ -12,8 +12,24 @@ stations = stations[["STAID","STANAME                                 "]]
 
 @app.route("/")
 def home():
-
     return render_template("home.html", data=stations.to_html())
+
+
+@app.route("/api/v1/<station>")
+def station_data(station):
+    station_nr = str(station).zfill(6)
+    df = pd.read_csv("data\TG_STAID" +station_nr + ".txt", skiprows=20, parse_dates=["    DATE"])
+    reasult = df.to_dict(orient="records")
+    return jsonify(reasult)
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def station_data_by_year(station, year):
+    station_nr = str(station).zfill(6)
+    df = pd.read_csv("data\TG_STAID" +station_nr + ".txt", skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    reasult = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return jsonify(reasult)
 
 
 @app.route("/api/v1/<station>/<date>")
