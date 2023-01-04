c2ee63e151039db650665b56d4ed8249c5a3b8a8
Canthar90 <d.cieslak90@gmail.com>
2023-01-04 12:52:04 +0100

    Added table of stations in home page

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ import pandas as pd
 app = Flask(__name__)
 Bootstrap(app)
 
+stations = pd.read_csv("data\stations.txt", skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
